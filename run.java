e80b4e9f47b6d93483cb2fcfa29bc1df237b4592
mazjap <jordan.c4922@gmail.com>
2019-07-19 22:44:07 -0600

    Delete run.java

--- run.java
+++
@@ -1,7 +0,0 @@
-
-public class run {
-    public static void main (String [] args) {
-        System.out.println("Enter the text you would like to turn into color:");
-        ColorPicker c = new ColorPicker();
-    }
-}
